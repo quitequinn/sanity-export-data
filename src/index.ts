979fa5c8ad749ffb3039d0c9ee26368a93d85a76
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 10:41:03 -0700

    Add main export file for NPM package

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Sanity Export Data - Main Export
+ * Flexible data export utility for Sanity Studio
+ */
+
+export { default as ExportData } from './ExportData'
+export { default } from './ExportData'
+
+// Export types if needed
+export type {
+  ExportDataProps,
+  ExportFormat,
+  ExportResult
+} from './ExportData'
